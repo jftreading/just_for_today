a872319fa4ba2edec488d139c6b093cb030c6173
Rye Salvador <machinehead449@gmail.com>
2015-09-27 07:50:08 +0000

    Enable Javascript

--- src/com/wordpress/jftreading/Home.java
+++ src/com/wordpress/jftreading/Home.java
@@ -1,5 +1,6 @@
 package com.wordpress.jftreading;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -7,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 
+@SuppressLint("SetJavaScriptEnabled")
 public class Home extends Fragment {
 	private View fragmentView;
 	private WebView browser;
@@ -15,6 +17,7 @@ public class Home extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		fragmentView = inflater.inflate(R.layout.home, container, false);
 		browser = (WebView) fragmentView.findViewById(R.id.webkit);
+		browser.getSettings().setJavaScriptEnabled(true);
 		browser.loadUrl("https://jftreading.wordpress.com/");
 		return fragmentView;
 	}
