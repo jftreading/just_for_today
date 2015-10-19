0089a229490880f0d77803b8559e5e7057f0e61c
Rye Salvador <machinehead449@gmail.com>
2015-10-19 04:32:29 +0000

    Attach a Copyright notice to source files

--- src/com/wordpress/jftreading/WebViewInterface.java
+++ src/com/wordpress/jftreading/WebViewInterface.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2015 jftreading.wordpress.com.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     jftreading.wordpress.com - email: machinehead449@gmail.com
+ ******************************************************************************/
 package com.wordpress.jftreading;
 
 public interface WebViewInterface {
